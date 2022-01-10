2e6e070512336b97d195f8d09d95de323116a460
wikili202 <wikili202@gmail.com>
2022-01-10 07:38:32 +0100

    Add files via upload

---
+++ wk_logo/config.js
@@ -0,0 +1,11 @@
+// Bienvenue dans la configuration du logo.
+// Welcome in the config file for the logo.
+
+logo = "" // Lien du Logo -- Link of the logo
+
+// ---------------------
+load();
+
+function load() {
+	document.getElementById('logo').src = logo;
+}
---
+++ wk_logo/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+
+ui_page 'index.html'
+
+files {
+	'index.css',
+	'config.js',
+	'index.html'
+}
